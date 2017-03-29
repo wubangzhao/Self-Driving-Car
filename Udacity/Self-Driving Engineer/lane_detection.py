63824f75a4498917ee7a3bad6f3ba10f0d03d94c
wubangzhao <always.why@live.cn>
2017-03-29 11:20:17 +0200

    Update lane_detection.py

--- Udacity/Self-Driving Engineer/lane_detection.py
+++ Udacity/Self-Driving Engineer/lane_detection.py
@@ -1,5 +1,10 @@
 
+blur_ksize = 5
+
+
 
 def process_an_image(img):
   # 1. Gray Scale Transformation
   gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+  # 2. Gaussian Smoothing
+  blur_gray = cv2.GaussianBlur(gray, (blur_ksize, blur_ksize), 0, 0)
