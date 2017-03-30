76688acb3609aba20fe52aaadfed7ecb3423a5b0
wubangzhao <always.why@live.cn>
2017-03-30 11:19:19 +0200

    Update lane_detection.py

--- Udacity/Self-Driving Engineer/lane_detection.py
+++ Udacity/Self-Driving Engineer/lane_detection.py
@@ -1,6 +1,12 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mplimg
+import numpy as np
+import cv2
 
-blur_ksize = 5
 
+blur_ksize = 5  # Gaussian blur kernel size
+canny_lthreshold = 50  # Canny edge detection low threshold
+canny_hthreshold = 150  # Canny edge detection high threshold
 
 
 def process_an_image(img):
@@ -8,3 +14,5 @@ def process_an_image(img):
   gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
   # 2. Gaussian Smoothing
   blur_gray = cv2.GaussianBlur(gray, (blur_ksize, blur_ksize), 0, 0)
+  # 3. Canny Edge Detection
+  edges = cv2.Canny(blur_gray, canny_lthreshold, canny_hthreshold)
